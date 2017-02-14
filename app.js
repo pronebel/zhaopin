a81ee84e1deb60e8b457193f637d41156e048290
linrui <670390939@qq.com>
2017-02-14 17:46:51 +0800

    add search limit

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
-var $=require('utils/util.js');
+var $ = require('utils/util.js');
 
 App({
-  onLaunch: function () {
-    //获取本地存储的workplace,如为空,采用当地为workplace
-    var workplace = wx.getStorageSync('workplace');
-    if(!workplace){
-      //还没存储workplace，应该为第一次访问
-    }else{
-      this.globalData.workplace=workplace;
-      console.log(workplace);
-    }
-    $.getLocationAddress(this);
-    
-    //获取本地存储的cityList
-    var cityList=wx.getStorageSync('cityList');
-    if(!cityList){
-      $.getCityList();  //调用腾讯地图开放平台获取城市列表 保存在本地存储
-      this.globalData.cityList=wx.getStorageSync('cityList');
-    }else{
-      this.globalData.cityList=cityList;
-    }
+    onLaunch: function() {
+        //获取本地存储的workplaceCity,如为空,采用当地为workplaceCity
+        var workplaceCity = wx.getStorageSync('workplaceCity');
+        if (!workplaceCity) {
+            //还没存储workplaceCity，应该为第一次访问
+        } else {
+            this.globalData.workplaceCity = workplaceCity;
+            console.log(workplaceCity);
+        }
+        $.getLocationAddress(this);
 
-  },
-  globalData:{
-    userInfo:null,
-    location:'',
-    workplace:'',
-    cityList:[]
-  }
+        //获取本地存储的cityList
+        var cityList = wx.getStorageSync('cityList');
+        if (!cityList) {
+            $.getCityList(); //调用腾讯地图开放平台获取城市列表 保存在本地存储
+            this.globalData.cityList = wx.getStorageSync('cityList');
+        } else {
+            this.globalData.cityList = cityList;
+        }
+
+        var workplaceDistrict = wx.getStorageSync('workplaceDistrict');
+        console.log(workplaceDistrict);
+        this.globalData.workplaceDistrict = workplaceDistrict;
+
+    },
+    globalData: {
+        userInfo: null,
+        location: '',
+        workplaceCity: '',
+        workplaceDistrict: '',
+        cityList: []
+    }
 })
