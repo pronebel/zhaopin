3d8b098c318be1ccc717112e2dbbe08bd628eae4
linrui <670390939@qq.com>
2017-03-05 00:30:44 +0800

    主页添加 drawerlayout

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,54 @@
 var app = getApp();
-
+const MENU_WIDTH_SCALE = 0.82;
+const FAST_SPEED_SECOND = 300;
+const FAST_SPEED_DISTANCE = 5;
+const FAST_SPEED_EFF_Y = 50;
 Page({
 	data: {
-
+		ui: {
+			windowWidth: 0,
+			menuWidth: 0,
+			offsetLeft: 0,
+			tStart: true,
+			offsetTop: 0,
+		},
+		jobList: [{
+			name: '前端',
+			salary: '8-10k',
+			campany: 'xxx公司',
+			city: '深圳',
+			degree: '本科',
+			scale: '100-500人',
+			type: '移动互联网',
+			releaseDate: '01月11日'
+		}, {
+			name: '前端',
+			salary: '8-10k',
+			campany: 'xxx公司',
+			city: '深圳',
+			degree: '本科',
+			scale: '100-500人',
+			type: '移动互联网',
+			releaseDate: '01月11日'
+		}, {
+			name: '前端',
+			salary: '8-10k',
+			campany: 'xxx公司',
+			city: '深圳',
+			degree: '本科',
+			scale: '100-500人',
+			type: '移动互联网',
+			releaseDate: '01月11日'
+		}, {
+			name: '前端',
+			salary: '8-10k',
+			campany: 'xxx公司',
+			city: '深圳',
+			degree: '本科',
+			scale: '100-500人',
+			type: '移动互联网',
+			releaseDate: '01月11日'
+		}]
 	},
 	toSearch: () => {
 		wx.navigateTo({
@@ -11,13 +57,17 @@ Page({
 	},
 	onLoad: function() {
 		var that = this
-
-	},
-	touchmove: function(e) {
-		console.log(e);
-	},
-	touchend: function(e) {
-		console.log(e);
+		try {
+			let res = wx.getSystemInfoSync()
+			this.windowWidth = res.windowWidth;
+			this.data.ui.menuWidth = this.windowWidth * MENU_WIDTH_SCALE;
+			this.data.ui.offsetLeft = 0;
+			this.data.ui.windowWidth = res.windowWidth;
+			this.setData({
+				ui: this.data.ui,
+				windowHeight: res.windowHeight
+			})
+		} catch (e) {}
 	},
 	toJobDetail: function() {
 		wx.navigateTo({
@@ -35,5 +85,124 @@ Page({
 				})
 			}
 		})
+	},
+	handlerStart(e) {
+		let {
+			clientX,
+			clientY
+		} = e.touches[0];
+		this.tapStartX = clientX;
+		this.tapStartY = clientY;
+		this.tapStartTime = e.timeStamp;
+		this.startX = clientX;
+		this.startY = clientY;
+		this.data.ui.tStart = true;
+		this.setData({
+			ui: this.data.ui
+		})
+	},
+	handlerMove(e) {
+		let {
+			clientX,
+			clientY
+		} = e.touches[0];
+		let {
+			ui
+		} = this.data;
+		let offsetX = this.startX - clientX;
+		let offsetY = this.startY - clientY;
+		this.startY = clientY;
+		this.startX = clientX;
+		if (Math.abs(offsetY) * 1.5 > Math.abs(offsetX))
+			return;
+		ui.offsetLeft -= offsetX;
+		// ui.offsetTop -= offsetY;
+		if (ui.offsetLeft <= 0) {
+			ui.offsetLeft = 0;
+		} else if (ui.offsetLeft >= ui.menuWidth) {
+			ui.offsetLeft = ui.menuWidth;
+		}
+		this.setData({
+			ui: ui
+		})
+	},
+	handlerEnd(e) {
+		this.data.ui.tStart = false;
+		this.setData({
+			ui: this.data.ui
+		})
+		let {
+			ui
+		} = this.data;
+		let {
+			clientX,
+			clientY
+		} = e.changedTouches[0];
+		let endTime = e.timeStamp;
+		let offsetX = this.startX - clientX;
+		let offsetY = this.startY - clientY;
+		if (Math.abs(offsetY) * 1.5 > Math.abs(offsetX))
+			return;
+		//快速滑动
+		if (endTime - this.tapStartTime <= FAST_SPEED_SECOND) {
+			//向左
+			if (this.tapStartX - clientX > FAST_SPEED_DISTANCE) {
+				ui.offsetLeft = 0;
+			} else if (this.tapStartX - clientX < -FAST_SPEED_DISTANCE && Math.abs(this.tapStartY - clientY) < FAST_SPEED_EFF_Y) {
+				ui.offsetLeft = ui.menuWidth;
+			} else {
+				if (ui.offsetLeft >= ui.menuWidth / 2) {
+					ui.offsetLeft = ui.menuWidth;
+				} else {
+					ui.offsetLeft = 0;
+				}
+			}
+		} else {
+			if (ui.offsetLeft >= ui.menuWidth / 2) {
+				ui.offsetLeft = ui.menuWidth;
+			} else {
+				ui.offsetLeft = 0;
+			}
+		}
+		this.setData({
+			ui: ui
+		})
+	},
+	shadowcatch: function() {
+		return;
+	},
+	showScan: function() {
+		wx.scanCode({
+			success: function(res) {
+				console.log(res);
+			}
+		})
+	},
+	handlerPageTap(e) {
+		let {
+			ui
+		} = this.data;
+		if (ui.offsetLeft != 0) {
+			ui.offsetLeft = 0;
+			this.setData({
+				ui: ui
+			})
+		}
+	},
+	showBars(e) {
+		this.setData({
+			'ui.offsetLeft': 1
+		})
+		setTimeout(function() {
+			let {
+				ui
+			} = this.data;
+			if (ui.offsetLeft == 1) {
+				ui.offsetLeft = ui.menuWidth;
+				this.setData({
+					ui: ui
+				})
+			}
+		}.bind(this), 30)
 	}
 })
--- pages/msg/msg.js
+++ pages/msg/msg.js
@@ -1,27 +1,10 @@
-var app=getApp();
+var app = getApp();
 
 Page({
-	data:{
-		
-	},
-	onLoad:function(){
-		wx.getLocation({
-			type: 'gcj02', //返回可以用于wx.openLocation的经纬度
-			success: function(res) {
-				var latitude = res.latitude
-				var longitude = res.longitude
-				wx.openLocation({
-					latitude: latitude,
-					longitude: longitude,
-					scale: 16
-				})
-			}``
-		})
+	data: {
 
-		// wx.chooseLocation({
-		// 	success:function(res){
-		// 		console.log(res);
-		// 	}
-		// })
+	},
+	onLoad: function() {
+
 	}
 })
