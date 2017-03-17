33b2ad7907dba3dc9dec91beeb9779c98443e785
linrui <670390939@qq.com>
2017-03-17 23:18:13 +0800

    新增collection job

---
+++ zhaopin-server/src/main/java/com/demo/service/CollectionService.java
@@ -0,0 +1,29 @@
+package com.demo.service;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.demo.dao.CollectionDao;
+import com.demo.model.*;
+
+@Service
+public class CollectionService {
+
+	@Resource
+	private CollectionDao cd;
+	
+	public List<Collection> getCollections(Collection c){
+		return cd.getCollections(c);
+	}
+	
+	public boolean deleteCollection(Collection c){
+		return cd.deleteCollection(c);
+	}
+	
+	public boolean addCollection(Collection c){
+		return cd.addCollection(c);
+	}
+}
