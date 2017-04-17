cc76ed542e2d9c48a009b979ec92813985937d77
linrui <670390939@qq.com>
2017-04-17 15:21:12 +0800

    1

--- zhaopin-user/configs/serverConfig.js
+++ zhaopin-user/configs/serverConfig.js
@@ -1,11 +1,6 @@
 const SERVER_CONFIG = {
-<<<<<<< HEAD
-	server:'localhost:8080/zhaopin',
+	server: 'https://zwill.space/zhaopin',
 	port: 80,
-=======
-	server: 'http://localhost:8080/zhaopin',
-	port: 8080,
->>>>>>> 7c3b5d26c4576ca14b0248eda920654be896fd81
 	appName: 'zhaopin',
 	ws: 'wss://zwill.space/zhaopin/ws',
 	serverName: 'localhost',
